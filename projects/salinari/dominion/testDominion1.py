aa582fd12d667c553007244a28fdc5382e9467ba
Ricky Salinas <rsalinas6721@gmail.com>
2020-01-19 21:37:23 -0500

    Update

--- projects/salinari/dominion/testDominion1.py
+++ projects/salinari/dominion/testDominion1.py
@@ -20,40 +20,12 @@ else:
     nV=8
 nC = -10 + 10 * len(player_names)
 
-#REFACTORED CODE
+'''
+# REFACTORED CODE
+    This portion of the refactored code utilises the testUtility python script
+'''
 box = testUtility.GetBoxes(nV)
 #Define box
-'''
-box = {}
-box["Woodcutter"]=[Dominion.Woodcutter()]*10
-box["Smithy"]=[Dominion.Smithy()]*10
-box["Laboratory"]=[Dominion.Laboratory()]*10
-box["Village"]=[Dominion.Village()]*10
-box["Festival"]=[Dominion.Festival()]*10
-box["Market"]=[Dominion.Market()]*10
-box["Chancellor"]=[Dominion.Chancellor()]*10
-box["Workshop"]=[Dominion.Workshop()]*10
-box["Moneylender"]=[Dominion.Moneylender()]*10
-box["Chapel"]=[Dominion.Chapel()]*10
-box["Cellar"]=[Dominion.Cellar()]*10
-box["Remodel"]=[Dominion.Remodel()]*10
-box["Adventurer"]=[Dominion.Adventurer()]*10
-box["Feast"]=[Dominion.Feast()]*10
-box["Mine"]=[Dominion.Mine()]*10
-box["Library"]=[Dominion.Library()]*10
-box["Gardens"]=[Dominion.Gardens()]*nV
-box["Moat"]=[Dominion.Moat()]*10
-box["Council Room"]=[Dominion.Council_Room()]*10
-box["Witch"]=[Dominion.Witch()]*10
-box["Bureaucrat"]=[Dominion.Bureaucrat()]*10
-box["Militia"]=[Dominion.Militia()]*10
-box["Spy"]=[Dominion.Spy()]*10
-box["Thief"]=[Dominion.Thief()]*10
-box["Throne Room"]=[Dominion.Throne_Room()]*10
-'''
-
-
-
 
 supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
                 3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
@@ -65,7 +37,8 @@ supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
 boxlist = [k for k in box]
 
 '''
-    REFACTORED CODE
+# REFACTORED CODE
+    random.shuffle(boxlist) was commented out
     Game Box is not shuffled and in turn will cause the cards to consistently remain the same throughout miultiple games.
 '''
 #random.shuffle(boxlist)
@@ -79,7 +52,14 @@ supply["Silver"]=[Dominion.Silver()]*40
 supply["Gold"]=[Dominion.Gold()]*30
 supply["Estate"]=[Dominion.Estate()]*nV
 supply["Duchy"]=[Dominion.Duchy()]*nV
-supply["Province"]=[Dominion.Province()]*nV
+
+
+'''
+# REFACTORED CODE
+    supply["Province"]=[Dominion.Province()]*nV was refactored to supply["Province"]=[Dominion.Province()]*1
+    This bug maeks the game only have 1 Province card available.
+'''
+supply["Province"]=[Dominion.Province()]*1
 supply["Curse"]=[Dominion.Curse()]*nC
 
 #initialize the trash
--- projects/salinari/dominion/testDominion2.py
+++ projects/salinari/dominion/testDominion2.py
@@ -7,6 +7,7 @@ Created on Saturday Jan 12 14:50:00 2020
 
 import Dominion
 import random
+import testUtility
 from collections import defaultdict
 
 #Get player names
@@ -19,41 +20,15 @@ else:
     nV=8
 nC = -10 + 10 * len(player_names)
 
-#REFACTORED CODE
+'''
+# REFACTORED CODE
+    This portion of the refactored code utilises the testUtility python script
+'''
 box = testUtility.GetBoxes(nV)
 #Define box
-'''
 
-box = {}
-box["Woodcutter"]=[Dominion.Woodcutter()]*10
-box["Smithy"]=[Dominion.Smithy()]*10
-box["Laboratory"]=[Dominion.Laboratory()]*10
-box["Village"]=[Dominion.Village()]*10
-box["Festival"]=[Dominion.Festival()]*10
-box["Market"]=[Dominion.Market()]*10
-box["Chancellor"]=[Dominion.Chancellor()]*10
-box["Workshop"]=[Dominion.Workshop()]*10
-box["Moneylender"]=[Dominion.Moneylender()]*10
-box["Chapel"]=[Dominion.Chapel()]*10
-box["Cellar"]=[Dominion.Cellar()]*10
-box["Remodel"]=[Dominion.Remodel()]*10
-box["Adventurer"]=[Dominion.Adventurer()]*10
-box["Feast"]=[Dominion.Feast()]*10
-box["Mine"]=[Dominion.Mine()]*10
-box["Library"]=[Dominion.Library()]*10
-box["Gardens"]=[Dominion.Gardens()]*nV
-box["Moat"]=[Dominion.Moat()]*10
-box["Council Room"]=[Dominion.Council_Room()]*10
-box["Witch"]=[Dominion.Witch()]*10
-box["Bureaucrat"]=[Dominion.Bureaucrat()]*10
-box["Militia"]=[Dominion.Militia()]*10
-box["Spy"]=[Dominion.Spy()]*10
-box["Thief"]=[Dominion.Thief()]*10
-box["Throne Room"]=[Dominion.Throne_Room()]*10
 
-'''
-
-supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
+supply_order = {0:['Curse','Copper','Province'],2:['Estate','Cellar','Chapel','Moat'],
                 3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
                 4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
                 5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
@@ -63,14 +38,26 @@ supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
 #Pick 10 cards from box to be in the supply.
 boxlist = [k for k in box]
 random.shuffle(boxlist)
-random10 = boxlist[:10]
+
+'''
+# REFACTORED CODE
+    random10 = boxlist[:10] was modified to random10 = boxlist[:25]
+    This makes every card accessible in game instead of only 10
+'''
+random10 = boxlist[:25]
 supply = defaultdict(list,[(k,box[k]) for k in random10])
 
 
 #The supply always has these cards
 supply["Copper"]=[Dominion.Copper()]*(60-len(player_names)*7)
-supply["Silver"]=[Dominion.Silver()]*40
-supply["Gold"]=[Dominion.Gold()]*30
+
+'''
+# REFACTORED CODE
+    supply["Silver"]=[Dominion.Silver()]*40 and supply["Gold"]=[Dominion.Gold()]*30 aare commented out
+    This prevents the two cards from being aailable in the game
+'''
+#supply["Silver"]=[Dominion.Silver()]*40
+#supply["Gold"]=[Dominion.Gold()]*30
 supply["Estate"]=[Dominion.Estate()]*nV
 supply["Duchy"]=[Dominion.Duchy()]*nV
 supply["Province"]=[Dominion.Province()]*nV
